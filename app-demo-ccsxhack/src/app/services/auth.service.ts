b76297f524150ea522bd3241ec1c18a459c76e6e
Jenderson Quintero <jendersonq.20@gmail.com>
2022-11-13 16:04:35 +0100

    Servicio de login creado e imports para la base de datos

--- app-demo-ccsxhack/src/app/services/auth.service.ts
+++ app-demo-ccsxhack/src/app/services/auth.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { Auth, signInWithPopup, GoogleAuthProvider } from '@angular/fire/auth';
+import { signOut } from 'firebase/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  constructor(private auth: Auth) {}
 
-  constructor() { }
+  loginWithGoogle() {
+    return signInWithPopup(this.auth, new GoogleAuthProvider());
+  }
+
+  logout() {
+    return signOut(this.auth);
+  }
 }
--- app-demo-ccsxhack/src/environments/environment.prod.ts
+++ app-demo-ccsxhack/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
+  firebase: {
+    projectId: 'fir-ccsxhack',
+    appId: '1:756570404817:web:2e914553c240335f2292f0',
+    storageBucket: 'fir-ccsxhack.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBieREAOhGFFgJ6uvUsR20FfkUy5_DqY2U',
+    authDomain: 'fir-ccsxhack.firebaseapp.com',
+    messagingSenderId: '756570404817',
+    measurementId: 'G-CV3XPTKV6V',
+  },
   production: true
 };
