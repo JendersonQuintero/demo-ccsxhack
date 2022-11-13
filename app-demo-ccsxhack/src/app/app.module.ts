e86ce095ba9ee81ee4502ec7f9f521c952d255cd
Jenderson Quintero <jendersonq.20@gmail.com>
2022-11-13 14:50:07 +0100

    conexión con la base de datos

--- app-demo-ccsxhack/src/app/app.module.ts
+++ app-demo-ccsxhack/src/app/app.module.ts
@@ -1,12 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// ************* FIREBASE ******************
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LandingPageComponent } from './views/landing-page/landing-page.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  declarations: [AppComponent, LandingPageComponent, NavbarComponent],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- app-demo-ccsxhack/src/environments/environment.ts
+++ app-demo-ccsxhack/src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBieREAOhGFFgJ6uvUsR20FfkUy5_DqY2U',
+    authDomain: 'fir-ccsxhack.firebaseapp.com',
+    projectId: 'fir-ccsxhack',
+    storageBucket: 'fir-ccsxhack.appspot.com',
+    messagingSenderId: '756570404817',
+    appId: '1:756570404817:web:2e914553c240335f2292f0',
+    measurementId: 'G-CV3XPTKV6V',
+  },
 };
 
 /*
